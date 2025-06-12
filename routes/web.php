3ac08b5189df5f2fa9d8ebe0a1a3e2f3945de0dd
CesiGomez-9 <cesi.gomez@unah.hn>
2025-06-12 05:08:43 -0600

    Se agregan las rutas

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,28 @@ Route::post('/empleados', [EmpleadoController::class, 'store'])->name('empleados
 Route::get('/empleados/create', [EmpleadoController::class, 'create'])->name('empleados.create');
 Route::resource('empleados', EmpleadoController::class);
 
+Route::controller(InventarioController::class)->group(function () {
+    Route::get('/inventarios', 'index')->name('inventarios.index');
+    Route::get('/inventarios/{id}', 'show')->name('inventarios.show')->whereNumber('id');
+    Route::get('/inventarios/crear', 'create')->name('inventarios.create'); /*->middleware('auth');*/
+    Route::post('/inventarios/crear', 'store')->name('inventarios.store'); #->middleware('auth');
+    /*Route::get('/inventarios/{id}/editar', 'edit')->name('inventarios.edit')->whereNumber('id'); #->middleware('auth');
+    Route::put('/inventarios/{id}/editar', 'update')->name('inventarios.update')->whereNumber('id'); #->middleware('auth');
+    Route::delete('/inventarios/{id}/eliminar', 'destroy')->name('inventarios.destroy')->whereNumber('id'); #->middleware('auth');
+    */
+});
+Route::controller(InventarioController::class)->group(function () {
+    Route::get('/inventarios', 'index')->name('inventarios.index');
+    Route::get('/inventarios/{id}', 'show')->name('inventarios.show')->whereNumber('id');
+    Route::get('/inventarios/crear', 'create')->name('inventarios.create'); /*->middleware('auth');*/
+    Route::post('/inventarios/crear', 'store')->name('inventarios.store'); #->middleware('auth');
+    /*Route::get('/inventarios/{id}/editar', 'edit')->name('inventarios.edit')->whereNumber('id'); #->middleware('auth');
+    Route::put('/inventarios/{id}/editar', 'update')->name('inventarios.update')->whereNumber('id'); #->middleware('auth');
+    Route::delete('/inventarios/{id}/eliminar', 'destroy')->name('inventarios.destroy')->whereNumber('id'); #->middleware('auth');
+    */
+});
+
+Route::get('/', [ServicioController::class, 'index'])->name('servicios.index');
+Route::post('/servicios', [ServicioController::class, 'store'])->name('servicios.store');
+Route::delete('/servicios/{id}', [ServicioController::class, 'destroy'])->name('servicios.destroy');
+Route::get('/catalogo', [ServicioController::class, 'catalogo'])->name('servicios.catalogo');
