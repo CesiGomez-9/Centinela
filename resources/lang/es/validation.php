b488a02886c134ea53d02348210e4360be396f64
Maria Alejandra <mchacona@unah.hn>
2025-06-12 00:53:13 -0600

    Se actualizo la crecacion de inventario

--- resources/lang/es/validation.php
+++ resources/lang/es/validation.php
@@ -1,17 +1,10 @@
 <?php
 
 return [
-    'required' => 'El campo :attribute es obligatorio.',
+    'required' => 'La informacion de :attribute es obligatorio.',
     'unique' => 'El valor de :attribute ya está en uso.',
-    'max' => [
-        'string' => 'El campo :attribute no puede tener más de :max caracteres.',
-    ],
-    'min' => [
-        'string' => 'El campo :attribute debe tener al menos :min caracteres.',
-        'numeric' => 'El campo :attribute debe ser al menos :min.',
-    ],
-    'numeric' => 'El campo :attribute debe ser un número.',
-    'integer' => 'El campo :attribute debe ser un número entero.',
+    'numeric' => 'La informacion de :attribute debe ser un número.',
+    'integer' => 'La informacion de :attribute debe ser un número entero.',
 
     'attributes' => [
         'codigo' => 'código',
