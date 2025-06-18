88bffc1c7cd567a0185c15f433c6d95da9ea4185
angelessalgado-1 <angeles.salgado@unah.hn>
2025-06-18 09:34:18 -0600

    Cambios validacion y otros en proveedores

--- resources/lang/es/validation.php
+++ resources/lang/es/validation.php
@@ -1,16 +1,10 @@
 <?php
 
 return [
-    'required' => 'La informacion de :attribute es obligatorio.',
-    'unique' => 'El valor de :attribute ya está en uso.',
-    'numeric' => 'La informacion de :attribute debe ser un número.',
-    'integer' => 'La informacion de :attribute debe ser un número entero.',
 
-    'attributes' => [
-        'codigo' => 'código',
-        'nombre' => 'nombre del producto',
-        'descripcion' => 'descripción',
-        'cantidad' => 'cantidad',
-        'precio_unitario' => 'precio unitario',
-    ],
+    'unique' => 'El valor de :attribute ya está en uso.',
+    'numeric' => 'El :attribute debe ser un número.',
+    'integer' => 'EL :attribute debe ser un número entero.',
+
+
 ];
