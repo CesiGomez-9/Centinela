7b075980e898b422acd1cfd92b3ea28b83b75e95
MariaAlejandraChacon <mchacona@unah.hn>
2025-06-19 07:50:31 -0600

    Actualizacion completada

--- routes/web.php
+++ routes/web.php
@@ -45,8 +45,4 @@ Route::get('/catalogo', [ServicioController::class, 'catalogo'])->name('servicio
 
 
 
-Route::get('/Proveedores/crear', [\App\Http\Controllers\ProveedorController::class, 'create'])->name('Proveedores.create');
-Route::post('/Proveedores/crear', [\App\Http\Controllers\ProveedorController::class, 'store'])->name('Proveedores.store');
-Route::get('/Proveedores', [\App\Http\Controllers\ProveedorController::class, 'index'])->name('Proveedores.indexProveedor');
-Route::get('/Proveedores/crear', [\App\Http\Controllers\ProveedorController::class, 'create'])->name('Proveedores.nuevo');
 
