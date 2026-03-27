322a0451c16b205f96976f40e8c42d9434b690ed
1Minsk0 <rodriguez.angel@unah.hn>
2026-03-27 12:06:15 -0600

    Update

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\Permission\Models\Role as SpatieRole;
-use Spatie\Permission\Traits\HasRoles;
 
 class Role extends SpatieRole
 {
-    use HasFactory, HasRoles;
+    use HasFactory;
 
     protected $fillable = ['name', 'descripcion'];
 
